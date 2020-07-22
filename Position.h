375f7d5f7581f0c7e1d23571c7ef50b7f13e01d2
ishaan-bose <68555412+ishaan-bose@users.noreply.github.com>
2020-07-22 21:33:31 +0530

    Update Position.h

--- Position.h
+++ Position.h
@@ -1,12 +1,28 @@
 #pragma once
+
 class Position
 {
 	//variable name explains the purpose of the variable
-	int world_x;
-	int world_y;
 
-	int screenx;
-	int screeny;
+	int world_x = 0;
+	int world_y = 0;
+
+	int screenx = 0;
+	int screeny = 0;
 
 	//yes, that's it. Just four variables variables
+
+	int screen_width = 40;
+	int screen_height = 20;
+
+	//the above variable are the maximum width and height, because
+	//I want the width and height to be constant,case closed.
+
+	//functions:
+
+public:
+	//this function only changes the world coordinates of the object
+	//it doesn't move it by displaying it moving, it doesn't print 
+	//the object to the console
+	void Move(int x, int y);
 };
